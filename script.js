eebcff3fe61c71a293da23bd045248528ec83729
OrbitalBuzzsaw <38387870+OrbitalBuzzsaw@users.noreply.github.com>
2025-02-15 18:45:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW9jaHJpc3Rlc2VuIiwiYSI6ImNtNzVzM3ozNzAxMzMycnB1Ymswd3pxdmgifQ.OXmeKBE7RZ3VT88AiV93Nw';
+
+const map = new mapboxgl.Map({
+    container: 'map', // HTML container ID
+    style: 'mapbox://styles/aochristesen/cm75sbdga00i401quarbffk7tL', // Mapbox Studio style URL
+    center: [-100, 40], // Initial centre 
+    zoom: 4
+});
+
+// zoom and rotation controls
+map.addControl(new mapboxgl.NavigationControl());
+
+map.on('load', function () {
+    // Add layer
+    map.addSource('layer1', {
+        type: 'geojson',
+        data: 'data/mbta_stations.geojson'
+    });
+
+    map.addLayer({
+        id: 'layer1',
+        type: 'fill',
+        source: 'layer1',
+        paint: {
+            'fill-color': '#ff0000',
+            'fill-opacity': 0.5
+        }
+    });
+
+    // Add second layer
+    map.addSource('layer2', {
+        type: 'geojson',
+        data: 'data/mbta_lines.geojson'
+    });
+
+    map.addLayer({
+        id: 'layer2',
+        type: 'circle',
+        source: 'layer2',
+        paint: {
+            'circle-radius': 6,
+            'circle-color': '#0000ff'
+        }
+    });
+});
